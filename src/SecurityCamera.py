8868c2358f1eaa358a5237dc1e587ab8af1e2dd0
Sakshi Sinha <sakshisinha8602@gmail.com>
2022-05-05 01:52:38 -0700

    sakshi

--- src/SecurityCamera.py
+++ src/SecurityCamera.py
@@ -1,4 +1,4 @@
-from MotionDetector import MotionDetector
+#from MotionDetector import MotionDetector
 from camera.RaspberryCamera import RaspberryCamera
 from runner.TimeRunner import TimeRunner
 from runner.AlwaysOnRunner import AlwaysOnRunner
@@ -6,7 +6,70 @@ from database.S3database import S3database
 from time import sleep
 import argparse
 
+import numpy as np
+from camera.MotionCamera import MotionCamera
+from database.Database import Database
+from runner.Runner import Runner
+
+import io
+import time
+import asyncio
+from typing import Optional
+from ffmpeg import FFmpeg
+from time import sleep, perf_counter
+from threading import Thread
+
+def file_save(camera, database,runner)
+
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    encoded_filename = '{}.h264'.format(timestamp)
+
+    print('Saving to database')
+    database.save_footage(recorded_stream, encoded_filename)
+
+    camera.start_recording()
+    camera.start_preview()
+    startTime = time.time()
+
+def run(camera, database,runner):
+
+    image_pair = [None, camera.capture_next_image()]
+    last_motion_triggered = time.time()
+    database.connect()
+
+    runner.start()
+    startTime = time.time()
+    camera.start_recording()
+    camera.start_preview()
+
+    while runner.should_run():
+
+        if time.time() - startTime > 0.5:
+            camera.stop_recording()
+            camera.stop_preview()
+            recorded_stream = camera.get_video_stream()
+            recorded_stream.seek(0)
+            file_save(camera, database,runner)
+
+    camera.close()
+    database.close()
+
 if __name__ == '__main__':
+
+    start_time = perf_counter()
+
+    # create two new threads
+    t1 = Thread(target=run, args=[camera, database,runner])
+    t2 = Thread(target=file_save, args=[camera, database,runner])
+
+    # start the threads
+    t1.start()
+    t2.start()
+
+    # wait for the threads to complete
+    t1.join()
+    t2.join()
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '-t',
@@ -35,4 +98,8 @@ if __name__ == '__main__':
 
     s3db = S3database()
     detector = MotionDetector(cam, s3db, runner)
-    detector.run()
+    detector.run(camera, database,runner)
+
+    end_time = perf_counter()
+
+    print(f'It took {end_time- start_time: 0.2f} second(s) to complete.')
